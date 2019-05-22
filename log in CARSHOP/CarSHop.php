5ea0c51a9f4c10feca5ab12d313d84571f8da3d2
Albionaa <44812037+Albionaa@users.noreply.github.com>
2019-05-22 23:54:27 +0200

    Add files via upload

---
+++ log in CARSHOP/CarSHop.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>log in CARSHOP</title>
+    <link rel="stylesheet" href="css.carshop.css">
+</head>
+<body>
+    <div class="slider">
+     <div class="load"> 
+     </div>   
+     <div class="content">
+         <div class="principal">
+                         
+         </div>
+     </div>
+    </div>
+</body>
+</html>
